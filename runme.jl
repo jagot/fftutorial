23f0eea26fd42c5e23529600d169c7702545ed9d
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2023-02-22 15:20:28 +0100

    Allow avoiding environment instantiation

--- runme.jl
+++ runme.jl
@@ -1,6 +1,18 @@
 using Pkg
 Pkg.activate(".")
-Pkg.instantiate()
+
+do_instantiate = true
+
+for a in ARGS
+    if a ∈ ["-ni", "--no-instantiate"]
+        global do_instantiate = false
+    end
+end
+
+if do_instantiate
+    @info "Instantiating environment"
+    Pkg.instantiate()
+end
 
 using Pluto
 Pluto.run(notebook="./fft-tutorial.jl")
